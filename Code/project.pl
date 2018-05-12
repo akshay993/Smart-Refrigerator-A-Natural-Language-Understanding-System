6f4ad8f6401f53b13389343dd130256844ee96b1
Shubham4792 <shubhampandey4792@gmail.com>
2018-05-11 21:02:24 -0400

    added model checker ynq

--- Code/project.pl
+++ Code/project.pl
@@ -331,6 +331,339 @@ rule(Y,[whpr(X^Y),be,pp2(X)]).
 rule(rc(Y,[X]),[rel,s(Y,[X])]).
 rule(rc(Y,[]),[rel,vp(Y,[])]).
 rule(n(X^and(Y,Z)),[n(X^Y),rc(X^Z,[])]).
+rule(n(X^and(Y,Z)),[n(X^Y),rc(Z,[X])])
+% ===========================================================
+% Main loop:
+% 1. Repeat "input-response" cycle until input starts with "bye"
+%    Each "input-response" cycle consists of:
+% 		1.1 Reading an input string and convert it to a tokenized list
+% 		1.2 Processing tokenized list
+% ===========================================================
+
+chat:-
+ repeat,
+   readinput(Input),
+   process(Input),
+  (Input = [bye| _] ),!.
+
+
+% ===========================================================
+% Read input:
+% 1. Read char string from keyboard.
+% 2. Convert char string to atom char list.
+% 3. Convert char list to lower case.
+% 4. Tokenize (based on spaces).
+% ===========================================================
+
+readinput(TokenList):-
+   read_line_to_codes(user_input,InputString),
+   string_to_atom(InputString,CharList),
+   string_lower(CharList,LoweredCharList),
+   tokenize_atom(LoweredCharList,TokenList).
+
+
+% ===========================================================
+%  Process tokenized input
+% 1. Parse morphology and syntax, to obtain semantic representation
+% 2. Evaluate input in the model
+% If input starts with "bye" terminate.
+% ===========================================================
+
+process(Input):-
+	parse(Input,SemanticRepresentation),
+	modelchecker(SemanticRepresentation,Evaluation),
+	respond(Evaluation),!,
+	nl,nl.
+
+process([bye|_]):-
+   write('> bye!').
+
+
+% ===========================================================
+%  Parse:
+% 1. Morphologically parse each token and tag it.
+% 2. Add semantic representation to each tagged token
+% 3. Obtain FOL representation for input sentence
+% ===========================================================
+
+%parse(Input, SemanticRepresentation):-
+% ...
+
+%% Added SR parser
+
+parse(Sentence,M):-
+        srparse([],Sentence,M).
+
+srparse([X],[],[X]).
+
+srparse([Z,Y,X|MoreStack],Words,M):-
+      rule(LHS,[X,Y,Z]),
+      srparse([LHS|MoreStack],Words,M).
+
+srparse([Y,X|MoreStack],Words,M):-
+       rule(LHS,[X,Y]),
+       srparse([LHS|MoreStack],Words,M).
+
+srparse([X|MoreStack],Words,M):-
+       rule(LHS,[X]),
+       srparse([LHS|MoreStack],Words,M).
+
+
+srparse(Stack,[Word|Words],M):-
+        lex(X,Word),
+        srparse([X|Stack],Words,M).
+
+%% End of parser
+
+% ===========================================================
+% Grammar
+% 1. List of lemmas
+% 2. Lexical items
+% 3. Phrasal rules
+% ===========================================================
+
+% --------------------------------------------------------------------
+% Lemmas are uninflected, except for irregular inflection
+% lemma(+Lemma,+Category)
+% --------------------------------------------------------------------
+
+
+lemma(is,be).
+lemma(was,be).
+lemma(are,be).
+
+
+%%%%%%%%%% ------------ Lemmas
+
+%% Determiners
+lemma(a,dtexists).
+lemma(an,dtexists).
+lemma(the,dtexists).
+lemma(some,dtexists).
+lemma(no,dtnotexists).
+lemma(each,dtforall).
+lemma(all,dtforall).
+lemma(every,dtforall).
+
+%% Numerals
+lemma(one,one).
+lemma(two,two).
+lemma(three,three).
+lemma(four,four).
+lemma(five,five).
+lemma(six,six).
+lemma(seven,seven).
+lemma(eight,eight).
+lemma(nine,nine).
+lemma(ten,ten).
+
+%% Nouns
+lemma(egg,n).
+lemma(shelf,n).
+lemma(fridge,n).
+lemma(container,n).
+lemma(sandwich,n).
+lemma(meat,n).
+lemma(tofu,n).
+lemma(apple,n).
+lemma(ham,n).
+lemma(bowl,n).
+lemma(vegetable,n).
+lemma(banana,n).
+lemma(watermelon,n).
+lemma(almond,n).
+lemma(milk,n).
+lemma(popsicle,n).
+lemma(can,n).
+lemma(skim,n).
+lemma(box,n).
+
+%% Proper Nouns
+lemma(tom,pn).
+lemma(mia,pn).
+lemma(sam,pn).
+
+%% Adjectives
+lemma(blue,adj).
+lemma(yellow,adj).
+lemma(white,adj).
+lemma(green,adj).
+lemma(top,adj).
+lemma(middle,adj).
+lemma(bottom,adj).
+lemma(red,adj).
+lemma(empty,adj).
+
+
+%% Verbs
+lemma(expire,iv).
+lemma(drink,tv).
+lemma(drank,tv).
+lemma(drunk,tv).
+lemma(contain,tv).
+lemma(have,tv).
+lemma(had,tv).
+lemma(has,tv).
+lemma(eat,tv).
+lemma(ate,tv).
+lemma(took,tv).
+lemma(take,tv).
+lemma(put,dtv).
+
+%% Prepositions
+lemma(in,p).
+lemma(inside,p).
+lemma(under,p).
+lemma(with,p).
+lemma(on,p).
+lemma(to,p).
+
+%% Relative Clauses
+lemma(that,rel).
+lemma(which,rel).
+lemma(who,rel).
+
+
+%% Auxilary Verbs (be)
+lemma(were,be).
+lemma(will,be).
+lemma(has,be).
+lemma(had,be).
+lemma(have,be).
+lemma(did,be).
+lemma(do,be).
+
+%% WHPR
+lemma(who,whpr).
+lemma(what,whpr).
+
+%%%%%%%%%% ------------ End Lemmas
+
+
+% --------------------------------------------------------------------
+% Constructing lexical items:
+% word = lemma + suffix (for "suffix" of size 0 or bigger)
+% --------------------------------------------------------------------
+
+
+
+%%%%%%%%%% ------------ Lexicons
+
+%DT_ForAll
+lex(dt((X^P)^(X^Q)^forall(X,imp(P,Q))),Word):- lemma(Word,dtforall).
+
+%DT_Exists
+lex(dt((X^P)^(X^Q)^exists(X,and(P,Q))),Word):-lemma(Word,dtexists).
+
+%DT_NotExisits
+lex(dt((X^P)^(X^Q)^not(exists(X,and(P,Q)))), Word):-lemma(Word,dtnotexists).
+
+%Noun
+lex(n(X^P),Word):- lemma(Word,n), P =.. [Word,X].
+
+%Proper_Noun
+lex(pn((Word^X)^X),Word):- lemma(Word,pn).
+
+%Intransitive_Verb
+lex(iv(X^P,[]),Word):-lemma(Word,iv), P=.. [Word,X].
+
+%Transitive_Verb
+lex(tv(K^W^P,[]),Word):-lemma(Word,tv), P=.. [Word,K,W].
+
+%Adjective
+lex(adj((X^P)^X^and(P,Q)),Word):-lemma(Word,adj), Q=.. [Word,X].
+
+%Preposition
+lex(p2(K^W^P),Word):-lemma(Word,p), P=.. [Word,K,W].
+
+lex(p1((Y^Z)^Q^(X^P)^and(P,Q)),Word):- lemma(Word,p), Z=.. [Word,X,Y].
+
+%WHPR
+lex(whpr((X^P)^q(X,and(person(X),P))),who):- lemma(who,whpr).
+lex(whpr((X^P)^q(X,and(thing(X),P))),what):- lemma(what,whpr).
+
+%Numerals
+lex(dt((X^P)^(X^Q)^two(X,and(P,Q))),two):- lemma(two,two).
+lex(dt((X^P)^(X^Q)^three(X,and(P,Q))),three):- lemma(three,three).
+lex(dt((X^P)^(X^Q)^four(X,and(P,Q))),four):- lemma(four,four).
+lex(dt((X^P)^(X^Q)^five(X,and(P,Q))),five):- lemma(five,five).
+lex(dt((X^P)^(X^Q)^six(X,and(P,Q))),six):- lemma(six,six).
+lex(dt((X^P)^(X^Q)^seven(X,and(P,Q))),seven):- lemma(seven,seven).
+lex(dt((X^P)^(X^Q)^eight(X,and(P,Q))),eight):- lemma(eight,eight).
+lex(dt((X^P)^(X^Q)^nine(X,and(P,Q))),nine):- lemma(nine,nine).
+lex(dt((X^P)^(X^Q)^ten(X,and(P,Q))),ten):- lemma(ten,ten).
+
+%VACP
+lex(p1((Y^Z)^Q^(X^P)^and(P,Q)),Word):- lemma(Word,vacp), Z=.. [Word,X,Y].
+
+%Auxilary_Verb
+lex(be,Word) :- lemma(Word,be).
+lex(rel,Word):- lemma(Word,rel).
+
+lex(there,there).
+
+
+%%%%%%%%%% ------------ Lexicons
+
+%%%%%%%%%% ------------ Lexicons with inflections
+
+lex(iv(X^P,[]),Y):-lemma(Word,iv),atom_concat(Word,M,Y),suffix(M),P=.. [Word,X].
+lex(iv(X^P,[]),Y):-lemma(Word,iv),atom_concat(Temp,e,Word),sub_atom(Y,_,_,_,Temp),atom_concat(Temp,ing,Y),P=.. [Word,X].
+
+lex(tv(K^W^P,[]),Y):-lemma(Word,tv),atom_concat(Word,M,Y),suffix(M),P=.. [Word,K,W].
+lex(tv(K^W^P,[]),Y):-lemma(Word,tv),atom_concat(Temp,e,Word),sub_atom(Y,_,_,_,Temp),atom_concat(Temp,ing,Y),P=.. [Word,K,W].
+
+lex(be,Y):-lemma(Word,be),atom_concat(Word,M,Y),suffix(M).
+lex(be,Y):-lemma(Word,be),atom_concat(Temp,e,Word),sub_atom(Y,_,_,_,Temp),atom_concat(Temp,ing,Y).
+
+lex(n(X^P),Y):- lemma(Word,n),atom_concat(Word,s,Y), P =.. [Word,X].
+lex(n(X^P),Y):- lemma(Word,n),atom_concat(Word,es,Y), P =.. [Word,X].
+
+
+%%%%%%%%%% ------------ End Lexicons with inflections
+
+
+% ...
+
+% --------------------------------------------------------------------
+% Suffix types
+% --------------------------------------------------------------------
+suffix(s).
+suffix(es).
+suffix(ed).
+suffix(d).
+suffix(ing).
+
+% ...
+
+% --------------------------------------------------------------------
+% Phrasal rules
+% rule(+LHS,+ListOfRHS)
+% --------------------------------------------------------------------
+
+%%%% -------------------- Rules
+
+
+rule(np((X^Y)^exists(X,and(Z,Y))),[n(X^Z)]).
+rule(np(Y),[dt(X^Y),n(X)]).
+rule(np(X),[pn(X)]).
+rule(n(A^C),[n(A^B),pp1((A^B)^C)]).
+rule(n(A),[adj(B^A),n(B)]).
+rule(pp1(C),[p1(A^B^C),np(A^B)]).
+rule(pp2(A^B),[p2(A^C),np(C^B)]).
+rule(vp(A^B,[]),[tv(A^C,[]),np(C^B)]).
+rule(ynq(Y),[be, np(X^Y),vp(X,[])]).
+rule(s(B,[]),[np(A^B),vp(A,[])]).
+rule(vp(X,WH),[iv(X,WH)]).
+rule(s(X,WH),[vp(X,WH)]).
+
+rule(ynq(Y),[be, np(X^Y),pp2(X)]).
+rule(Y,[whpr(X^Y),vp(X,[])]).
+rule(Y,[whpr(X^Y),be,pp2(X)]).
+
+rule(rc(Y,[X]),[rel,s(Y,[X])]).
+rule(rc(Y,[]),[rel,vp(Y,[])]).
+rule(n(X^and(Y,Z)),[n(X^Y),rc(X^Z,[])]).
 rule(n(X^and(Y,Z)),[n(X^Y),rc(Z,[X])]).
 
 rule(iv(X^A,[Y]),[tv(X^Y^A,[])]).
@@ -341,8 +674,8 @@ rule(inv_s(Y,[WH]),[be, np(X^Y),vp(X,[WH])]).
 rule(np(X),[there,np(X)]).
 
 %notworking
-rule(ynq(Y),[be, np(X^Y),np(X)]).
-rule(ynq(X^A),[be, np(X^T),np(T^A)]).
+% rule(ynq(X),[be, np(X)]).
+% rule(ynq(X^A),[be, np(X^T),np(T^A)]).
 
 %%%% ------------------- End of Rules
 
@@ -357,12 +690,11 @@ rule(ynq(X^A),[be, np(X^T),np(T^A)]).
 
 model([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z],[[bowl,[a,b]],[yellow,[b]],[egg,[c,d]],[milk,[l]],[bottom,[i]],[container,[e,f,g]],
 			[white,[e,f]],[banana,[j,k]],[expire,[l]],[on,[[e,i],[f,i]]],[contain,[[a,c],[b,d],[e,j],[n,p]]],[shelf,[h,i,m]],[box,[n,o]],[green,[n]],
-			[ham,[p]]]).
+			[ham,[p]],[inside,[[c,a],[d,b],[j,e],[p,n]]]]).
 
-modelchecker([s(B,[])],Result):- nl,sat([],B,G),write(G),Result is 1.
-
-modelchecker([q(B,[])],Result):- write(B),nl,sat([],B,G),Result is G.
-modelchecker([ynq(B)],Result):- write(B),nl,sat([],B,G),write("reach"),write(G),nl,Result is 1.
+modelchecker([s(B,[])],Result):- write(B),nl,sat([],B,G),write(G),nl,Result is 1.
+modelchecker([q(M)],Result):- write(M),nl,sat([],M,G),write(G),nl,Result is 1.
+modelchecker([ynq(B)],Result):- write(B),nl,sat([],B,G),write(G),nl,Result is 1.
 			
 % ==================================================
 % Function i
